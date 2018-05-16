49cbc2a3953acece795b9e4b2167685385111e6a
BZIvanov <ivanov.biser@mail.bg>
2018-05-16 09:54:01 +0300

    Update 04.filterAge.js

--- A_JS-Fundamentals/01.IntroToJS/04.filterAge.js
+++ A_JS-Fundamentals/01.IntroToJS/04.filterAge.js
@@ -2,7 +2,7 @@ function filterByAge(minAge, nameA, ageA, nameB, ageB) {
     let personA = { name: nameA, age: ageA}
     let personB = { name: nameB, age: ageB}
 
-    if(personA.age >= minAge)
+    if(personA['age'] >= minAge)
         console.log(personA)
     if(personB.age >= minAge)
         console.log(personB)
