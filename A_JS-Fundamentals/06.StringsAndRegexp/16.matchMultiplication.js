1f901b8b95db5d28a261b157d542c4daf3a1745d
BZIvanov <ivanov.biser@mail.bg>
2018-06-02 23:50:05 +0300

    Update 16.matchMultiplication.js

--- A_JS-Fundamentals/06.StringsAndRegexp/16.matchMultiplication.js
+++ A_JS-Fundamentals/06.StringsAndRegexp/16.matchMultiplication.js
@@ -1,4 +1,5 @@
 function performMultiplications(text) {
+    // callback function takes as first parameter the whole match then each capturing group
     text = text.replace(/(-?\d+)\s*\*\s*(-?\d+(\.\d+)?)/g, (match, num1, num2) => Number(num1) * Number(num2));
     console.log(text);
 }
