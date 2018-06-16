466c933dda0c68ee532866bb0a04beb4ae3d267a
BZIvanov <ivanov.biser@mail.bg>
2018-06-16 18:15:55 +0300

    update

---
+++ B_JS-Advanced/02.DOM-Manipulations/09.fillDropDown.js
@@ -0,0 +1,13 @@
+function addItem() {
+    let textFrom = document.getElementById("newItemText")
+    let valueFrom = document.getElementById("newItemValue")
+
+    let option = document.createElement("option")
+    option.textContent = textFrom.value
+    option.value = valueFrom.value
+    let selectEl = document.getElementById("menu")
+    selectEl.appendChild(option)
+
+    textFrom.value = ""
+    valueFrom.value = ""
+}
