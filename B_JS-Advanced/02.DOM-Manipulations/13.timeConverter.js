75b1bb4636226465a77f66a23270aa1fd42bea35
BZIvanov <ivanov.biser@mail.bg>
2018-06-16 18:15:55 +0300

    update

---
+++ B_JS-Advanced/02.DOM-Manipulations/13.timeConverter.js
@@ -0,0 +1,48 @@
+function attachEventsListeners() {
+    let inputDays = document.getElementById('daysBtn')
+    let inputHours = document.getElementById('hoursBtn')
+    let inputMinutes = document.getElementById('minutesBtn')
+    let inputSeconds = document.getElementById('secondsBtn')
+
+    function calcDays() {
+        let mainValue = Number(document.getElementById('days').value)
+        let hours = document.getElementById('hours')
+        hours.value = mainValue * 24
+        let minutes = document.getElementById('minutes')
+        minutes.value = mainValue * 24 * 60
+        let seconds = document.getElementById('seconds')
+        seconds.value = mainValue * 24 * 60 * 60
+    }
+    function calcHours() {
+        let mainValue = Number(document.getElementById('hours').value)
+        let days = document.getElementById('days')
+        days.value = mainValue / 24
+        let minutes = document.getElementById('minutes')
+        minutes.value = mainValue * 60
+        let seconds = document.getElementById('seconds')
+        seconds.value = mainValue * 60 * 60
+    }
+    function calcMinutes() {
+        let mainValue = Number(document.getElementById('minutes').value)
+        let days = document.getElementById('days')
+        days.value = mainValue / 24 / 60
+        let hours = document.getElementById('hours')
+        hours.value = mainValue / 60
+        let seconds = document.getElementById('seconds')
+        seconds.value = mainValue * 60
+    }
+    function calcSeconds() {
+        let mainValue = Number(document.getElementById('seconds').value)
+        let days = document.getElementById('days')
+        days.value = mainValue / 24 / 60 / 60
+        let hours = document.getElementById('hours')
+        hours.value = mainValue / 60 / 60
+        let minutes = document.getElementById('minutes')
+        minutes.value = mainValue / 60
+    }
+
+    inputDays.addEventListener('click', calcDays)
+    inputHours.addEventListener('click', calcHours)
+    inputMinutes.addEventListener('click', calcMinutes)
+    inputSeconds.addEventListener('click', calcSeconds)
+}
---
+++ B_JS-Advanced/02.DOM-Manipulations/14.distanceConverter.js
@@ -0,0 +1,37 @@
+function attachEventsListeners() {
+    let inputValue = document.getElementById('inputDistance')
+    let btn = document.getElementById('convert')
+    btn.addEventListener('click', calc)
+
+    function calc() {
+        numberValue = Number(inputValue.value)
+        firstUnit = document.getElementById("inputUnits").selectedIndex
+        secondUnit = document.getElementById("outputUnits").selectedIndex
+
+        let convertToMeters
+        switch (firstUnit) {
+            case 0: convertToMeters = numberValue * 1000; break;
+            case 1: convertToMeters = numberValue * 1; break;
+            case 2: convertToMeters = numberValue * 0.01; break;
+            case 3: convertToMeters = numberValue * 0.001; break;
+            case 4: convertToMeters = numberValue * 1609.34; break;
+            case 5: convertToMeters = numberValue * 0.9144; break;
+            case 6: convertToMeters = numberValue * 0.3048; break;
+            case 7: convertToMeters = numberValue * 0.0254; break; 
+        }
+        
+        let result
+        switch (secondUnit) {
+            case 0: result = convertToMeters / 1000; break;
+            case 1: result = convertToMeters / 1; break;
+            case 2: result = convertToMeters / 0.01; break;
+            case 3: result = convertToMeters / 0.001; break;
+            case 4: result = convertToMeters / 1609.34; break;
+            case 5: result = convertToMeters / 0.9144; break;
+            case 6: result = convertToMeters / 0.3048; break;
+            case 7: result = convertToMeters / 0.0254; break;
+        }
+        let output = document.getElementById('outputDistance')
+        output.value = result
+    }
+}
