84d73ed421ade9772b31220d2d92891f6c376832
BZIvanov <ivanov.biser@mail.bg>
2018-11-06 08:01:05 +0200

    update

---
+++ B_JS-Advanced/07.Classes/05.pointDistance.js
@@ -0,0 +1,14 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(pointA, pointB) {
+        return Math.sqrt(Math.pow(pointB.x - pointA.x, 2) + Math.pow(pointB.y - pointA.y, 2));
+    }
+}
+
+let p1 = new Point(5, 5);
+let p2 = new Point(9, 8);
+console.log(Point.distance(p1, p2));
