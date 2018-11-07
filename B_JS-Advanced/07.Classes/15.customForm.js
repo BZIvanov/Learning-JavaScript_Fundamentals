d448d0714497c28eb40c8b717dce3dec9163ba5c
BZIvanov <ivanov.biser@mail.bg>
2018-11-08 00:44:32 +0200

    tasks

---
+++ B_JS-Advanced/07.Classes/15.customForm.js
@@ -0,0 +1,73 @@
+let result = (function () {
+    class TextBox {
+        constructor(selector, regex) {
+            this.selector = selector;
+            this._elements = $(selector);
+            this._invalidSymbols = regex;
+
+            let that = this;
+            $(selector).on('input change', function () {
+                that.value = $(this).val();
+            });
+        }
+
+        get value() {
+            return $(this.selector).val();
+        }
+
+        set value(value) {
+            $(this.selector).val(value);
+        }
+
+        get elements() {
+            return this._elements;
+        }
+
+        isValid() {
+            return !this._invalidSymbols.test($(this.selector).val());
+        }
+    }
+
+    class Form {
+        constructor(...textBoxes) {
+            for (let textBox of textBoxes) {
+                if (!textBox instanceof TextBox) {
+                    throw new Error('Argument is not an instance of TextBox class');
+                }
+            }
+            this._element = $('<div>').addClass('form');
+            this._textboxes = textBoxes;
+            for (let textBox of textBoxes) {
+                this._element.append($(textBox.selector));
+            }
+        }
+
+        submit() {
+            let formValidity = true;
+            for (let textBox of this._textboxes) {
+                if (textBox.isValid()) {
+                    $(textBox.selector).css('border', '2px solid green');
+                } else {
+                    $(textBox.selector).css('border', '2px solid red');
+                    formValidity = false;
+                }
+            }
+            return formValidity;
+        }
+
+        attach(selector) {
+            $(selector).append(this._element);
+        }
+    }
+
+    return {Textbox: TextBox, Form: Form};
+})();
+
+let Textbox = result.Textbox;
+let Form = result.Form;
+let username = new Textbox("#username",/[^a-zA-Z0-9]/);
+let password = new Textbox("#password",/[^a-zA-Z]/);
+username.value = "username";
+password.value = "password2";
+let form = new Form(username,password);
+form.attach("#root");
