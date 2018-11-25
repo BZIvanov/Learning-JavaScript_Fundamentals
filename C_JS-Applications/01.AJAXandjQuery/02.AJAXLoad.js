1f051a0c8b61f3469703c0c0350320e9e6a98564
BZIvanov <ivanov.biser@mail.bg>
2018-11-25 12:04:32 +0200

    js apps

---
+++ C_JS-Applications/01.AJAXandjQuery/02.AJAXLoad.js
@@ -0,0 +1,3 @@
+function loadHtml() {
+    $('#text').load('text.html');
+}
---
+++ C_JS-Applications/01.AJAXandjQuery/07.phonebook.js
@@ -0,0 +1,54 @@
+function attachEvents() {
+    const baseUrl = 'https://phonebook-nakov.firebaseio.com/phonebook';
+    $('#btnLoad').on('click', loadPhonebook);
+    $('#btnCreate').on('click', createContact);
+
+    function createContact() {
+        let person = $('#person').val();
+        let phone = $('#phone').val();
+        if (person !== '' && phone !== '') {
+            $.ajax({
+                method: "POST",
+                url: baseUrl + '.json',
+                data: JSON.stringify({ person, phone })
+            })
+            .then(loadPhonebook)
+            .catch(displayError)
+        }
+    }
+
+    function loadPhonebook() {    
+        $('#person').val('');
+        $('#phone').val('');
+        $.ajax({
+            method: "GET",
+            url: baseUrl + ".json"
+        })
+        .then(retrivePhonebook)
+        .catch(displayError);
+    }
+
+    function retrivePhonebook(contacts) {
+        $('#phonebook').empty();
+        for (let key in contacts) {
+            let person = contacts[key]['person'];
+            let number = contacts[key]['phone'];
+            let li = $(`<li>`).text(`${person}: ${number} `);
+            let delBtn = $(`<button>[Delete]</button>`);
+            delBtn.on('click', function () {
+                $.ajax({
+                    method: 'DELETE',
+                    url: baseUrl + '/' + key + '.json'
+                })
+                .then(loadPhonebook)
+                .catch(displayError)
+            });
+            li.append(delBtn);
+            $('#phonebook').append(li)
+        }
+    }
+
+    function displayError(err) {
+        $('#phonebook').append($('<li>').text("Error"));
+    }
+}
