9893843731b6277d85806a4e506bdf320f31dae9
BZIvanov <ivanov.biser@mail.bg>
2018-12-01 11:27:53 +0200

    update

---
+++ C_JS-Applications/03.Templating/01.app.js
@@ -0,0 +1,25 @@
+(async () => {
+    let data = await $.get('./01.data.json');
+    data = JSON.parse(data);
+    let dataAsObject = {contacts: data};
+    
+    const contactHtml = await $.get('./templates/contact.hbs');
+    let contactTemplate = Handlebars.compile(contactHtml);
+    $('#list').append(contactTemplate(dataAsObject));
+
+    const partialContactsHtml = await $.get('./templates/partials/personalContacts.hbs');
+    const partialInfoHtml = await $.get('./templates/partials/personalInfo.hbs');
+    Handlebars.registerPartial('personalContacts', partialContactsHtml);
+    Handlebars.registerPartial('personalInfo', partialInfoHtml);
+    const detailsHtml = await $.get('./templates/details.hbs');
+    const detailsTemplate = Handlebars.compile(detailsHtml);
+
+    $('.contact').on('click', function () {
+        $('.content > div').removeClass('contactSelected');
+        $(this).addClass('contactSelected');
+        let index = $(this).attr('data-id');
+        let result = detailsTemplate(data[index]);
+        $('#details > div').remove();
+        $('#details').append(result);
+    });
+})();
