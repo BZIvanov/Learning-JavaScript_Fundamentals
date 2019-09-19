bead6c4b2c006e14d26788a918d1d0786516792c
BZIvanov <ivanov.biser@mail.bg>
2019-09-19 12:14:37 +0300

    Reworked v1

--- A_JS-Fundamentals/01.IntroToJS/05.string1toN.js
+++
@@ -1,9 +0,0 @@
-function stringNumbers1toN (n) {
-    let str = ""
-    for (let i = 1; i <= Number(n); i++) {
-        str += i
-    }
-    console.log(str)
-}
-
-stringNumbers1toN('11')
--- A_JS-Fundamentals/01.IntroToJS/07.nextDay.js
+++
@@ -1,8 +0,0 @@
-function nextDay(year, month, day) {
-    let date = new Date(year, month - 1, day);
-    let oneDay = 24 * 60 * 60 * 1000 // miliseconds in 1 day
-    let nextDay = new Date(date.getTime() + oneDay)
-    console.log(nextDay.getFullYear() + '-' + (nextDay.getMonth() + 1) + '-' + nextDay.getDate())
-}
-
-nextDay(2016, 9, 30)
