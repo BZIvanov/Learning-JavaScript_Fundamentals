fc2cda82a6a2d231fba999ba8848abc6c159f556
BZIvanov <biser.z.ivanov@gmail.com>
2020-01-19 09:54:19 +0200

    added example with recursion

---
+++ B_JS-Advanced/04.AdvancedFunctions/recursionExample.js
@@ -0,0 +1,30 @@
+// we can use recursive function to get all the properties from an object for example, even if we don't know how deep nested they will be
+
+const recursionTree = {
+  name: 'John',
+  children: [
+    {
+      name: 'Jim',
+      children: []
+    },
+    {
+      name: 'Zoe',
+      children: [
+        { name: 'Kyle', children: [] },
+        { name: 'Sophia', children: [] }
+      ]
+    }
+  ]
+};
+
+function printChildrenRecursive(t) {
+  if (t.children.length === 0) {
+    return;
+  }
+  t.children.forEach(child => {
+    console.log(child.name);
+    printChildrenRecursive(child);
+  });
+}
+
+printChildrenRecursive(recursionTree);
