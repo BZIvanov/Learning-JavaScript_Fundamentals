cb7b7f9dc92f075caab3a3c06cf0028d9469502f
BZIvanov <biser.z.ivanov@gmail.com>
2020-08-02 19:43:38 +0300

    removed duplicated tasks

--- A_JS-Fundamentals/07.ObjectsAndJSON/07ObjectsAndJSON.js
+++ A_JS-Fundamentals/07.ObjectsAndJSON/07ObjectsAndJSON.js
@@ -80,24 +80,6 @@ function sumByTown(arr) {
 }
 sumByTown(['Sofia', 20, 'Varna', 3, 'Sofia', 5, 'Varna', 4]);
 
-// 04. Count words
-function countWords(input) {
-  input = input
-    .join('')
-    .split(/\W+/g)
-    .filter((x) => x !== '');
-  const result = {};
-  for (let i = 0; i < input.length; i++) {
-    if (result.hasOwnProperty(input[i])) {
-      result[input[i]] += 1;
-    } else {
-      result[input[i]] = 1;
-    }
-  }
-  console.log(JSON.stringify(result));
-}
-countWords(["Far too slow, you're far too slow."]);
-
 // 05. Count words with map
 function countWordsWithMap(input) {
   const myMap = new Map();
@@ -121,29 +103,6 @@ function countWordsWithMap(input) {
   }
 }
 countWordsWithMap(["Far too slow, you're far too slow.", 'hello']);
-
-// 06. Population in towns
-function populationInTowns(arr) {
-  const myMap = new Map();
-
-  for (const str of arr) {
-    const [town, population] = str.split(/\s*<->\s*/);
-    if (!myMap.has(town)) {
-      myMap.set(town, 0);
-    }
-    myMap.set(town, myMap.get(town) + +population);
-  }
-  for (const [key, value] of myMap) {
-    console.log(`${key} : ${value}`);
-  }
-}
-populationInTowns([
-  'Sofia <-> 1200000',
-  'Montana <-> 20000',
-  'New York <-> 10000000',
-  'Washington <-> 2345000',
-  'Las Vegas <-> 1000000',
-]);
 
 // 07. City markets
 function cityMarkets(arr) {
