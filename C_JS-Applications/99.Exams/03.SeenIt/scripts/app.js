f18a1b1583edc2f86fc80e0883f9a8bbc4172846
BZIvanov <biser.z.ivanov@gmail.com>
2020-09-10 23:50:56 +0300

    removed doc files

--- C_JS-Applications/99.Exams/03.SeenIt/scripts/app.js
+++ C_JS-Applications/99.Exams/03.SeenIt/scripts/app.js
@@ -1,30 +1,30 @@
 const handlers = {};
 
 $(() => {
-    const app = Sammy('#container', function () {
-        this.use("Handlebars", "hbs");
+  const app = Sammy('#container', function () {
+    this.use('Handlebars', 'hbs');
 
-        this.get('/index.html', handlers.displayHomePage);
-        this.get('#/home', handlers.displayHomePage);
+    this.get('/index.html', handlers.displayHomePage);
+    this.get('#/home', handlers.displayHomePage);
 
-        this.post('#/register', handlers.registerNewUser);
-        this.post('#/login', handlers.loginUser);
-        this.get('#/logout', handlers.logoutUser);
+    this.post('#/register', handlers.registerNewUser);
+    this.post('#/login', handlers.loginUser);
+    this.get('#/logout', handlers.logoutUser);
 
-        this.get('#/catalog', handlers.displayCatalog);
-        this.get('#/submitPost', handlers.displayNewPostForm);
-        this.post('#/submitPost', handlers.createPost);
+    this.get('#/catalog', handlers.displayCatalog);
+    this.get('#/submitPost', handlers.displayNewPostForm);
+    this.post('#/submitPost', handlers.createPost);
 
-        this.get('#/myPosts', handlers.displayMyPosts);
+    this.get('#/myPosts', handlers.displayMyPosts);
 
-        this.get('#/edit/:id', handlers.displayEditPostView);
-        this.post('#/edit/:id', handlers.submitEditPost);
-        this.get('#/comments/:id', handlers.displayComments);
-        this.get('#/catalog/:id', handlers.deletePost);
+    this.get('#/edit/:id', handlers.displayEditPostView);
+    this.post('#/edit/:id', handlers.submitEditPost);
+    this.get('#/comments/:id', handlers.displayComments);
+    this.get('#/catalog/:id', handlers.deletePost);
 
-        this.post('#/createComment/:id', handlers.createComment);
-        this.get('#/deleteComment/:postId/:commentId', handlers.deleteComment);
-    });
+    this.post('#/createComment/:id', handlers.createComment);
+    this.get('#/deleteComment/:postId/:commentId', handlers.deleteComment);
+  });
 
-    app.run();
+  app.run();
 });
--- C_JS-Applications/99.Exams/03.SeenIt/scripts/models/specifics.js
+++ C_JS-Applications/99.Exams/03.SeenIt/scripts/models/specifics.js
@@ -1,47 +1,44 @@
-let specifics = (() => {
-    function getAllPosts() {
-        // the endpoint means whole collection (query={}) and then sort the property ect in column _kmd, value -1 is for descending sort
-        const endpoint = 'posts?query={}&sort={"_kmd.ect": -1}'
-        return requester.get('appdata', endpoint, 'kinvey');
+const specifics = (() => {
+  function getAllPosts() {
+    // the endpoint means whole collection (query={}) and then sort the property ect in column _kmd, value -1 is for descending sort
+    const endpoint = 'posts?query={}&sort={"_kmd.ect": -1}';
+    return requester.get('appdata', endpoint, 'kinvey');
+  }
+
+  function uploadPostToDatabase(data) {
+    return requester.post('appdata', 'posts', 'kinvey', data);
+  }
+
+  function getAllMyPosts(username) {
+    // get specific username from column author in collection posts and sort descending by column _kmd and property ect
+    const endpoint = `posts?query={"author":"${username}"}&sort={"_kmd.ect":-1}`;
+    return requester.get('appdata', endpoint, 'kinvey');
+  }
+
+  function calcTime(dateIsoFormat) {
+    let diff = new Date() - new Date(dateIsoFormat);
+    diff = Math.floor(diff / 60000);
+    if (diff < 1) return 'less than a minute';
+    if (diff < 60) return diff + ' minute' + pluralize(diff);
+    diff = Math.floor(diff / 60);
+    if (diff < 24) return diff + ' hour' + pluralize(diff);
+    diff = Math.floor(diff / 24);
+    if (diff < 30) return diff + ' day' + pluralize(diff);
+    diff = Math.floor(diff / 30);
+    if (diff < 12) return diff + ' month' + pluralize(diff);
+    diff = Math.floor(diff / 12);
+    return diff + ' year' + pluralize(diff);
+
+    function pluralize(value) {
+      if (value !== 1) return 's';
+      else return '';
     }
+  }
 
-    function uploadPostToDatabase(data) {
-        return requester.post('appdata', 'posts', 'kinvey', data);
-    }
-
-    function getAllMyPosts(username) {
-        // get specific username from column author in collection posts and sort descending by column _kmd and property ect
-        const endpoint = `posts?query={"author":"${username}"}&sort={"_kmd.ect":-1}`
-        return requester.get('appdata', endpoint, 'kinvey');
-    }
-
-
-
-    function calcTime(dateIsoFormat) {
-        let diff = new Date - (new Date(dateIsoFormat));
-        diff = Math.floor(diff / 60000);
-        if (diff < 1) return 'less than a minute';
-        if (diff < 60) return diff + ' minute' + pluralize(diff);
-        diff = Math.floor(diff / 60);
-        if (diff < 24) return diff + ' hour' + pluralize(diff);
-        diff = Math.floor(diff / 24);
-        if (diff < 30) return diff + ' day' + pluralize(diff);
-        diff = Math.floor(diff / 30);
-        if (diff < 12) return diff + ' month' + pluralize(diff);
-        diff = Math.floor(diff / 12);
-        return diff + ' year' + pluralize(diff);
-
-        function pluralize(value) {
-            if (value !== 1) return 's';
-            else return '';
-        }
-    }
-    
-
-    return {
-        getAllPosts,
-        uploadPostToDatabase,
-        getAllMyPosts,
-        calcTime
-    }
+  return {
+    getAllPosts,
+    uploadPostToDatabase,
+    getAllMyPosts,
+    calcTime,
+  };
 })();
