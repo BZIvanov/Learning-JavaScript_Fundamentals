67405ca986e03f5783e4a9e0daaae1287cdcb8ec
BZIvanov <biser.z.ivanov@gmail.com>
2021-07-02 18:24:42 +0300

    call second example

--- B_JS-Advanced/14.More/call-example.js
+++ B_JS-Advanced/14.More/call-example.js
@@ -1,3 +1,5 @@
+// EXAMPLE 1
+
 const myObj = {
   name: 'Iva',
   greet() {
@@ -8,3 +10,16 @@ const myObj = {
 myObj.greet(); // Iva
 // with call we can provide what 'this' will be
 myObj.greet.call({ name: 'Hristina' }); // Hristina
+
+// EXAMPLE 2
+
+const user = {
+  firstName: 'Iva',
+};
+
+const greeter = function (text, punct) {
+  console.log(text + ' ' + this.firstName + punct);
+};
+
+// the first parameter is what we will use for this and every next parameter is parameter to the function
+greeter.call(user, 'Hello', '!'); // Hello Iva!
