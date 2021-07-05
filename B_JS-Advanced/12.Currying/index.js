ced3852b94a6afad95cedb9c9092529238bdf41a
BZIvanov <biser.z.ivanov@gmail.com>
2021-07-05 18:30:14 +0300

    another curry example

--- B_JS-Advanced/12.Currying/index.js
+++ B_JS-Advanced/12.Currying/index.js
@@ -1,5 +1,20 @@
+// sign that we should be using currying is when we are often repeating some parameters to the functions
+
+// EXAMPLE 1
 const curriedMultiply = (a) => (b) => a * b;
 const curriedMultiplyByFive = curriedMultiply(5);
 
 console.log(curriedMultiplyByFive(4)); // 20
 console.log(curriedMultiplyByFive(5)); // 25
+
+// EXAMPLE 2
+const greeter = function (greeting) {
+  return function (name) {
+    console.log(greeting + ' ' + name);
+  };
+};
+
+const welcomeGreet = greeter('Welcome');
+
+welcomeGreet('Iva'); // Welcome Iva
+welcomeGreet('Ani'); // Welcome Ani
